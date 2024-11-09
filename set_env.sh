f7f610b9243b0a4001f8a1f37e940c8a2f8e9680
diemnth10 <diemnth10@fpt.com>
2024-11-09 23:16:14 +0900

    second

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=Manchi3110
+export POSTGRES_HOST=cdr.cv7frt5be5cz.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::my-project3-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+export AWS_SDK_LOAD_CONFIG=true
+export NODE_OPTIONS=--openssl-legacy-provider
